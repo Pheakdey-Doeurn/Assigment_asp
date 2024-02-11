fd86847c79008820a0ca84f07a58d498d94ba467
Pheakdey-Douern <pheakdey.96kh@gmail.com>
2024-02-11 21:43:16 +0700

    first project

---
+++ AssignmentMe/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentMe.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string uname, string pw /*FormCollection values*/)
+        {
+            /*if (values["username"] == "admin" && values["password"] == "123")
+
+                return RedirectToAction("Products", "Product");
+                return Content("Invalid!");*/
+            if(uname=="Pheakdey" && pw == "123")
+            {
+                Session["username"] = uname;
+                return RedirectToAction("Products", "Product");
+            }
+            return Content("Login Failed!");
+
+
+        }
+    }
+}
---
+++ AssignmentMe/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentMe.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Products()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Products(FormCollection doc)
+        {
+            TempData["productName"] = doc["proName"];
+            TempData["price"] = doc["price"];
+            return RedirectToAction("Order", "Product");
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Order(int qty,int discount)
+        {
+            decimal price = 20;
+            TempData["qty"] = qty;
+            TempData["discount"] = discount;
+            TempData["total"] = (price * qty);
+            TempData["SubTotal"] = (price * qty) - discount;
+            
+            return RedirectToAction("Invoice", "Product");
+        }
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+    }
+}
